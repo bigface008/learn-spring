bb940967060fd5b088028e9db43f1282db82180c
bigface008 <bigface00@sjtu.edu.cn>
2018-07-01 12:32:12 +0800

    Initialization

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
